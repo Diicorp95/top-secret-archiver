6357ff07666e099f937340510f365708df9b09ca
Larry Holst <larry.holst@disroot.org>
2021-07-29 20:31:24 +0300

    Update top-secret-archiver.sh

--- top-secret-archiver.sh
+++ top-secret-archiver.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+# shellcheck disable=SC2016  # Adding intentional quotation marks
+# shellcheck disable=SC2031  # Change isn't lost because it's in IF
+# shellcheck disable=SC2034  # Unused variables left for readability
+# shellcheck disable=SC2089  # Adding intentional quotation marks
+# shellcheck disable=SC2090  # Adding intentional quotation marks
 
 # https://github.com/Diicorp95/top-secret-archiver
 echo "Top-Secret Archiver: Archiver of your top-secret files."
 echo -e "(c) Diicorp95. MIT License.\n"
+
+# Modules
+randpw(){ < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c72;} # https://www.howtogeek.com/howto/30184/10-ways-to-generate-a-random-password-from-the-command-line/
+prompt_confirm(){ while true;do read -r -n 1 -p "${1:-Continue?} [y/n]: " REPLY;case $REPLY in [yY])echo;return 1;;[nN])echo;return 0;;*)printf " \033[31m %s \n\033[0m" "invalid input";;esac;done;} # https://stackoverflow.com/a/32708121/13976788
 
 # https://stackoverflow.com/a/677212/13976788
 command -v 7z >/dev/null 2>&1 || { echo >&2 "7-Zip execution failed."; exit 1; }
@@ -16,11 +25,14 @@ then
 fi
 
 work_with_dir="$1"
-read -s -p "Set password for archive: " password
+password=
+read -s -p -r "Set password for archive: " password
 
-# If password is empty, then don't use -p option.
-if [ "$password" != '' ];
+# If password is not entered...
+if [ "$password" == '' ];
 then
+	prompt_confirm "Generate password?" || (password=randpw)
+else
 	option_password='-p"'
 	option_password+="$password"
 	option_password+='"'
@@ -43,6 +55,7 @@ new_filename+='_'
 new_filename+=$(date +%Y-%m-%d)
 
 # Add to archive
+
 7z a -t7z '"$new_filename"' '"$work_with_dir/*"' $option_password -mhe -mx=3 -ms=off
 # Test if everything is OK
 7z t '"$new_filename"'
